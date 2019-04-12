1387289270eb4788e8dddd062c917e4faa87a992
Justin Harthcock <harthcockbusiness@gmail.com>
2019-04-12 14:15:45 -0600

    first commit

---
+++ static/index.js
@@ -0,0 +1,11 @@
+// window.onload = function() {
+//     const vids = document.getElementsByTagName(‘whVid1’)
+//     for (let i = 0; i < vids.length; i++) {
+//       vids[i].addEventListener( ‘hover’, function(e) { 
+//         vids[i].play()
+//       })
+//       vids[i].addEventListener( ‘noHover’, function(e) {
+//         vids[i].pause()
+//       })
+//     }
+//    }
